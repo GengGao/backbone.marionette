GengGao/backbone.marionette

commit a790fb39b9d2bcfb12d6f8f8b6f8d6187e535ff4

Manually triggers configuration (verbose)

src/marionette.view.js
@@ -55,12 +55,23 @@ Marionette.View = Backbone.View.extend({
     // action and stop propagation of DOM events
     _.each(triggers, function(value, key){
 
+      var hasOptions = _.isObject(value);
+      var eventName = hasOptions ? value.event : value;
+
       // build the event handler function for the DOM event
       triggerEvents[key] = function(e){
 
         // stop the event in its tracks
-        if (e && e.preventDefault){ e.preventDefault(); }
-        if (e && e.stopPropagation){ e.stopPropagation(); }
+        if (e) {
+          var prevent = e.preventDefault;
+          var stop = e.stopPropagation;
+
+          var shouldPrevent = hasOptions ? value.preventDefault : prevent;
+          var shouldStop = hasOptions ? value.stopPropagation : stop;
+
+          if (shouldPrevent && prevent) { prevent(); }
+          if (shouldStop && stop) { stop(); }
+        }
 
         // build the args for the event
         var args = {
@@ -70,7 +81,7 @@ Marionette.View = Backbone.View.extend({
         };
 
         // trigger the event
-        this.triggerMethod(value, args);
+        this.triggerMethod(eventName, args);
       };
 
     }, this);

spec/javascripts/view.triggers.spec.js
@@ -137,4 +137,53 @@ describe("view triggers", function(){
     });
   });
 
+  describe("when triggers items are manually configured", function(){
+    var View = Backbone.Marionette.ItemView.extend({
+      triggers: {
+        "click .foo": {
+          event: "do:foo",
+          preventDefault: true,
+          stopPropagation: false
+        },
+        "click .bar": {
+          event: "do:bar",
+          stopPropagation: true
+        }
+      },
+
+      render: function(){
+        this.$el.html("<button class='foo'></button><a href='#' class='bar'>asdf</a>");
+      }
+    });
+
+    var view, fooEvent, barEvent;
+
+    beforeEach(function(){
+      view = new View();
+      view.render();
+
+      fooEvent = $.Event("click");
+      barEvent = $.Event("click");
+
+      spyOn(fooEvent, 'preventDefault');
+      spyOn(fooEvent, 'stopPropagation');
+
+      spyOn(barEvent, 'preventDefault');
+      spyOn(barEvent, 'stopPropagation');
+
+      view.$(".foo").trigger(fooEvent);
+      view.$(".bar").trigger(barEvent);
+    });
+
+    it("should prevent and don't stop the first view event", function(){
+      expect(fooEvent.preventDefault).toHaveBeenCalled();
+      expect(fooEvent.stopPropagation).not.toHaveBeenCalled();
+    });
+
+    it("should not prevent and stop the second view event", function(){
+      expect(barEvent.preventDefault).not.toHaveBeenCalled();
+      expect(barEvent.stopPropagation).toHaveBeenCalled();
+    });
+  });
+
 });

commit 2d6e1c27034ac322739dfa93cf28fc8dd8bd4b2c

[fix] ensure that the prevent default and stop prop occurs in the correct context
reported here https://github.com/marionettejs/backbone.marionette/issues/758#issuecomment-27213784
introduced in a790fb39b9d2bcfb12d6f8f8b6f8d6187e535ff4

src/marionette.view.js
@@ -69,8 +69,8 @@ Marionette.View = Backbone.View.extend({
           var shouldPrevent = hasOptions ? value.preventDefault : prevent;
           var shouldStop = hasOptions ? value.stopPropagation : stop;
 
-          if (shouldPrevent && prevent) { prevent(); }
-          if (shouldStop && stop) { stop(); }
+          if (shouldPrevent && prevent) { prevent.apply(e) }
+          if (shouldStop && stop) { stop.apply(e) }
         }
 
         // build the args for the event